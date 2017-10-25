80df5b4e51b4b7f7a0baa3c5903b10ecdb4a23f5
Michał Martyniak <michal.martyniak@linux.pl>
2017-10-25 16:40:23 +0200

    Hotfix bug related with paths, when YT video title contains spaces

--- scripts/download_from_yt_and_preprocess.sh
+++ scripts/download_from_yt_and_preprocess.sh
@@ -11,19 +11,19 @@ INPUT_FILE_PATH=$1
 OUTPUT_DIR=$2
 
 # Parse and download audio from input file containing links
-python src/utils/yt_downloader.py --input-file $INPUT_FILE_PATH --output-dir $OUTPUT_DIR
+#python src/utils/yt_downloader.py --input-file $INPUT_FILE_PATH --output-dir $OUTPUT_DIR
 
 for file in $OUTPUT_DIR/wav/*
 do
     # Get file name without the extension
     filename=$(basename "$file" .wav)
 
-    # Normalize the loudness
-    ffmpeg-normalize --no-prefix --force $OUTPUT_DIR/wav/*.wav 
+    # Normalize the loudness, override existing file
+    #ffmpeg-normalize --no-prefix --force $OUTPUT_DIR/wav/*.wav 
 
     # Prepare separate folder for each wav file
-    mkdir --parents $OUTPUT_DIR/spectrograms/$filename
+    mkdir --parents $OUTPUT_DIR/spectrograms/"$filename"
 
     # Run the preprocessor
-    python src/preprocessor.py --single-file-input $file --output-spectrograms-dir $OUTPUT_DIR/spectrograms/$filename
+    python src/preprocessor.py --single-file-input "$file" --output-spectrograms-dir $OUTPUT_DIR/spectrograms/"$filename"
 done
