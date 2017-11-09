11a63701fe1e0b467718cffa30196a550e41fe19
Michał Martyniak <michal.martyniak@linux.pl>
2017-11-09 21:09:26 +0100

    Refactor waveplot.js file

--- src/webapp/static/waveplot.js
+++ src/webapp/static/waveplot.js
@@ -1,79 +1,94 @@
-// Build components
-var fileSound;
-var wavesurfer = WaveSurfer.create({
-    container: "#waveform",
-    barWidth: 3,
-    height: 300,
-    progressColor: '#512da8',
-    skipLength: 30,
-    fillParent: true
+// Allow to execute when page is fully loaded
+$(function() {
+    var wavesurfer = initWavesurfer();
+    bindOnUploadChange(wavesurfer);
+    initWaveformControls(wavesurfer);
+    initZoomSlider(wavesurfer);
 });
 
-// Play uploaded audio
-document.getElementById("uploadedFileInput").onchange = function(e) {
 
-    $("#processingProgress").show();
+/**
+ * Bind action when file input element has changed its value
+ */
+function bindOnUploadChange(wavesurfer) {
+    $('#uploadedFileInput').on('change', function() {
+        $('#processingProgress').show();
 
-    // Put filename int readonly textfield, when file is chosen
-    document.getElementById("uploadFileName").value = this.files[0].name;
-
-    // Build player
-    var sound = document.getElementById("waveform");
-    fileSound = this.files[0];
-    sound.src = URL.createObjectURL(fileSound);
-
-    // Build zoom slider
-    var slider = document.querySelector("#zoomSlider");
-    slider.oninput = function() {
-        var zoomLevel = Number(slider.value);
-        wavesurfer.zoom(zoomLevel);
-    };
-
-    // Cleanup old regions
-    wavesurfer.clearRegions();
-
-    // Load file
-    wavesurfer.load(URL.createObjectURL(fileSound));
-
-    // Not really needed in this exact case, but since it is really important in other cases,
-    // don't forget to revoke the blobURI when you don't need it
-    sound.onend = function(e) {
-        URL.revokeObjectURL(this.src);
-    };
-};
-$(function() {
-    $("#backward").click(function() {
-        wavesurfer.skipBackward();
+        // Put filename int readonly textfield, when file is chosen
+        $('#uploadFileName').val(this.files[0].name);
+        fileUrl = URL.createObjectURL(this.files[0]);
+        wavesurfer.load(fileUrl);
+        wavesurfer.clearRegions();
     });
-    $("#togglePlay").click(function() {
-        wavesurfer.playPause();
+}
+
+/**
+ * Assign actions to player buttons
+ */
+function initWaveformControls(player) {
+    $('#backward').click(function() {
+        player.skipBackward();
     });
-    $("#forward").click(function() {
-        wavesurfer.skipForward();
+    $('#togglePlay').click(function() {
+        player.playPause();
     });
-    $("#toggleMute").click(function() {
-        wavesurfer.toggleMute();
+    $('#forward').click(function() {
+        player.skipForward();
+    });
+    $('#toggleMute').click(function() {
+        player.toggleMute();
+    });
+}
+
+/**
+ * Build wavesurfer object with all necessary options
+ * Add timeline and regions plugins
+ */
+function initWavesurfer() {
+    var wavesurfer = WaveSurfer.create({
+        container: '#waveform',
+        barWidth: 3,
+        height: 300,
+        progressColor: '#512da8',
+        skipLength: 30,
+        fillParent: true
     });
 
-    wavesurfer.on("ready", function() {
-        $("#processingProgress").hide();
-        $("#waveformSection").css({ opacity: 0.0, visibility: "visible" }).animate({ opacity: 1.0 }, "slow"); //.fadeToggle(); //css("visibility", "visible");
+    wavesurfer.on('ready', function() {
+        // Hide Progress bar
+        $('#processingProgress').hide();
+
+        // Animate with FadeIn effect
+        $('#waveformSection')
+            .css({ opacity: 0.0, visibility: 'visible' })
+            .animate({ opacity: 1.0 }, 'slow');
+
         // Add timeline
         var timeline = Object.create(WaveSurfer.Timeline);
         timeline.init({
             wavesurfer: wavesurfer,
-            container: "#waveform-timeline",
+            container: '#waveform-timeline',
             timeInterval: 5
         });
 
         // Enable creating regions by dragging
         wavesurfer.addRegion({
-            id: "startend",
-            start: 0, // time in seconds
-            end: 6, // time in seconds
+            id: 'startend',
+            start: 0, // in seconds
+            end: 6, // in seconds
             drag: true,
             resize: false,
-            color: "hsla(262, 52%, 47%, 0.48)"
+            color: 'hsla(262, 52%, 47%, 0.48)'
         });
     });
-});
+    return wavesurfer;
+}
+
+function initZoomSlider(player) {
+    var slider = document.querySelector("#zoomSlider");
+
+    slider.oninput = function() {
+        var zoomLevel = Number(slider.value);
+        player.zoom(zoomLevel);
+    };
+}
