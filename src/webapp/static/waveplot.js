7a06a13b940d6fb952c46a75106f23457e40758d
Michał Martyniak <michal.martyniak@linux.pl>
2017-11-09 19:46:30 +0100

    Clear selected waveform regions on new upload, add fade effect for box

--- src/webapp/static/waveplot.js
+++ src/webapp/static/waveplot.js
@@ -22,12 +22,15 @@ document.getElementById("uploadedFileInput").onchange = function(e) {
     fileSound = this.files[0];
     sound.src = URL.createObjectURL(fileSound);
 
+    // Build zoom slider
     var slider = document.querySelector("#zoomSlider");
-
     slider.oninput = function() {
         var zoomLevel = Number(slider.value);
         wavesurfer.zoom(zoomLevel);
     };
+
+    // Cleanup old regions
+    wavesurfer.clearRegions();
 
     // Load file
     wavesurfer.load(URL.createObjectURL(fileSound));
@@ -38,25 +41,39 @@ document.getElementById("uploadedFileInput").onchange = function(e) {
         URL.revokeObjectURL(this.src);
     };
 };
-
-wavesurfer.on("ready", function() {
-    $("#processingProgress").hide();
-
-    // Add timeline
-    var timeline = Object.create(WaveSurfer.Timeline);
-    timeline.init({
-        wavesurfer: wavesurfer,
-        container: "#waveform-timeline",
-        timeInterval: 5
+$(function() {
+    $("#backward").click(function() {
+        wavesurfer.skipBackward();
+    });
+    $("#togglePlay").click(function() {
+        wavesurfer.playPause();
+    });
+    $("#forward").click(function() {
+        wavesurfer.skipForward();
+    });
+    $("#toggleMute").click(function() {
+        wavesurfer.toggleMute();
     });
 
-    // Enable creating regions by dragging
-    wavesurfer.addRegion({
-        id: "startend",
-        start: 0, // time in seconds
-        end: 6, // time in seconds
-        drag: true,
-        resize: false,
-        color: "hsla(262, 52%, 47%, 0.48)"
+    wavesurfer.on("ready", function() {
+        $("#processingProgress").hide();
+        $("#waveformSection").css({ opacity: 0.0, visibility: "visible" }).animate({ opacity: 1.0 }, "slow"); //.fadeToggle(); //css("visibility", "visible");
+        // Add timeline
+        var timeline = Object.create(WaveSurfer.Timeline);
+        timeline.init({
+            wavesurfer: wavesurfer,
+            container: "#waveform-timeline",
+            timeInterval: 5
+        });
+
+        // Enable creating regions by dragging
+        wavesurfer.addRegion({
+            id: "startend",
+            start: 0, // time in seconds
+            end: 6, // time in seconds
+            drag: true,
+            resize: false,
+            color: "hsla(262, 52%, 47%, 0.48)"
+        });
     });
 });
